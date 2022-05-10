1151e7f01de167645b2b88f92bf3e37ef748b9f5
duynk13 <khuongduyprobg@gmail.com>
2022-05-10 07:56:36 +0700

    push code

--- debug_solver_2.py
+++ debug_solver_2.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -6,6 +7,7 @@ from libs.solver import Solver
 from utils import helpers
 from utils.transformer import get_transformer
 from expt.common import synthetic_params
+from classifiers.mlp import Net0
 
 
 # Get data
@@ -15,43 +17,50 @@ y = df['label'].to_numpy()
 X = df.drop('label', axis=1)
 X = transformer.transform(X)
 
+# Get model
+with open("results/run_0/checkpoints/mlp_synthesis_5.pickle", "rb") as f:
+    model = pickle.load(f)[0]
+y = model.predict(X)
+
 # Solver
-best_response = False
+best_response = True
 # theta_l = [0.1 * i for i in range(11)]
 theta_l = [1]
 # kernel_l = [0.1 * (i + 1) for i in range(21, 40)]
-kernel_l = [2.0]
+kernel_l = [0.5]
 for idx in range(len(theta_l)):
     for j in range(len(kernel_l)):
-        s = Solver(X, y, theta=1, kernel_width=kernel_l[j])
-        A, S, d = s.compute_matrix(np.array([1.5, 2]), X[y == 1])
-        w, v = s.find_eig(S)
+        s = Solver(model, X, y, theta=1, kernel_width=kernel_l[j])
+        recourse_set, X_diverse, X_other = s.generate_recourse(np.array([1.5, 2]), k=3)
+        # A, S, d = s.compute_matrix(np.array([1.5, 2]), X[y == 1])
+        # w, v = s.find_eig(S)
 
         # Best response or dp
-        if best_response:
-            z_p = s.best_response(w, v, d, k=5, period=20)
-        else:
-            z_p = s.dp(w, v, d, k=5, step_size=1, period=20)
+        # if best_response:
+        #     z_p = s.best_response(w, v, d, k=5, period=20)
+        # else:
+        #     z_p = s.dp(w, v, d, k=5, step_size=1, period=20)
     
-        z_prev = np.zeros(len(d))
+        # z_prev = np.zeros(len(d))
 
-        for i in range(20):
-            z_prev = np.logical_or(z_prev, z_p[i, :])
+        # for i in range(20):
+        #     z_prev = np.logical_or(z_prev, z_p[i, :])
     
-        idx_l = np.where(z_prev == 1)[0]
+        # idx_l = np.where(z_prev == 1)[0]
 
-        data = X[y == 1][np.where(z_prev == 1)[0]]
-        A, S, d = s.compute_matrix(X[0], data)
-        z = s.quad(S, d, 3)
-        idx = idx_l[np.where(z == 1)[0]]
+        # data = X[y == 1][np.where(z_prev == 1)[0]]
+        # A, S, d = s.compute_matrix(X[0], data)
+        # z = s.quad(S, d, 3)
+        # idx = idx_l[np.where(z == 1)[0]]
 
-        X_diverse = X[y == 1][idx]
-        X_other = X[y == 1][np.where(z_prev == 0)[0]]
+        # X_diverse = X[y == 1][idx]
+        # X_other = X[y == 1][np.where(z_prev == 0)[0]]
 
         # Plot
         fig, ax = plt.subplots()
         # ax.scatter(X[0][0], X[0][1], s=100, marker='*', label = 'x0')
         ax.scatter(1.5, 2, s=100, marker='*', label = 'x0')
+        ax.scatter(recourse_set[:, 0], recourse_set[:, 1], marker='o', c='#17becf', label = 'Recourse')
         ax.scatter(X_diverse[:, 0], X_diverse[:, 1], marker='o', label = 'Diverse')
         ax.scatter(X_other[:, 0], X_other[:, 1], marker='o', label = 'Other')
 
@@ -64,5 +73,5 @@ for idx in range(len(theta_l)):
 
         if not os.path.exists('results/figures/'):
             os.makedirs('results/figures/')
-        plt.savefig(f'results/figures/visualization_BR_{i}.jpg', dpi=500, transparent=True)
+        plt.savefig(f'results/figures/visualization_BR.jpg', dpi=500, transparent=True)
         plt.show()
--- libs/solver.py
+++ libs/solver.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.linalg import eigh
+from sklearn.neighbors import NearestNeighbors
 
 import gurobipy as grb
 
@@ -7,7 +8,8 @@ import gurobipy as grb
 class Solver(object):
     """ Class for optimization problem """
 
-    def __init__(self, data, labels, theta=0.5, kernel_width=1):
+    def __init__(self, model, data, labels, theta=0.5, kernel_width=1):
+        self.model = model
         self.data = data
         self.labels = labels
         self.theta = theta
@@ -16,7 +18,7 @@ class Solver(object):
         self.data_hat = self.data[self.labels == 1]
         self.N, self.dim = self.data_hat.shape
 
-    def quad(self, S, d, k):
+    def quad(self, S, d, k, cost_diverse=True):
         """ Solve quadratic program with gurobi
         """
         dim = len(d)
@@ -32,11 +34,18 @@ class Solver(object):
         z_sub = model.addMVar(1, vtype=grb.GRB.CONTINUOUS, name="zsub")
 
         # Set objectives
-        obj = (1 - self.theta) * d @ z - self.theta * z_sub
+        if cost_diverse:
+            obj = (1 - self.theta) * d @ z - self.theta * z_sub
+        else:
+            obj = -z_sub
         model.setObjective(obj, grb.GRB.MINIMIZE)
 
-         # Constraints
-        model.addConstr(z.sum() == k)
+        # Constraints
+        if cost_diverse:
+            model.addConstr(z.sum() == k)
+        else:
+            for i in range(0, k * k, k):
+                model.addConstr(z[i:i+k].sum() == 1)
         model.addConstr(z_sub == z @ S @ z)
 
         # Optimize
@@ -125,10 +134,10 @@ class Solver(object):
 
         # Best response and dp
         if best_response:
-            z_p = s.best_response(w, v, d, k=2*k, period=period)
+            z_p = self.best_response(w, v, d, k=2*k, period=period)
         else:
-            z_p = s.dp(w, v, d, k=2*k, step_size=1, period=period)
-
+            z_p = self.dp(w, v, d, k=2*k, step_size=1, period=period)
+        
         z_prev = np.zeros(len(d))
         for i in range(period):
             z_prev = np.logical_or(z_prev, z_p[i, :])
@@ -140,7 +149,41 @@ class Solver(object):
         z = self.quad(S, d, k)
 
         idx = idx_l[np.where(z == 1)[0]]
-        X_diverse = self.data[self.labels == 1]
-        X_other = X[y == 1][np.where(z_prev == 0)[0]]
+        X_diverse = self.data[self.labels == 1][idx]
+        X_other = self.data[self.labels == 1][np.where(z_prev == 0)[0]]
 
         return idx, X_diverse, X_other
+
+    def generate_recourse(self, x0, k, period=20, best_response=True):
+        idx, X_diverse, X_other = self.solve(x0, k, period, best_response)
+
+        knn = NearestNeighbors(n_neighbors=k)
+        knn.fit(self.data[self.labels == 0])
+        
+        recourse_set = []
+        for i in range(X_diverse.shape[0]):
+            idx = knn.kneighbors(X_diverse[i].reshape(1, -1), return_distance=False)
+            recourse_set_i = []
+            
+            for j in range(k):
+                best_x_b = None
+                best_dist = np.inf
+                
+                lambd_list = np.linspace(0, 1, 100)
+                for lambd in lambd_list:
+                    x_b = (1 - lambd) * X_diverse[i] + lambd * self.data[self.labels == 0][idx[0][j]]
+                    label = self.model.predict(x_b)
+                    if label == 1:
+                        dist = np.linalg.norm(x0 - x_b)
+                        if dist < best_dist:
+                            best_x_b = x_b
+                            best_dist = dist
+                
+                recourse_set_i.append(best_x_b)
+            recourse_set += recourse_set_i
+        
+        recourse_set = np.array(recourse_set)
+        A, S, d = self.compute_matrix(x0, recourse_set)
+        recourse_set = recourse_set[self.quad(S, d, k, cost_diverse=False) == 1]
+
+        return recourse_set, X_diverse, X_other
